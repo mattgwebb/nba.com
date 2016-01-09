f976c85dc57b51b3bf63256e05688f09b3115d29
ukswat <mattwebb.ynwa@gmail.com>
2016-01-09 15:09:17 +0100

    Modelo de Paises actualizado
    Conexion a BD

--- application/models/pais_model.php
+++ application/models/pais_model.php
@@ -2,14 +2,12 @@
 
 class Pais_model extends CI_Model {
 	
-	private $paises = array(
-    	"españa" => "España",
-    	"inglaterra" => "Inglaterra",
-    	"alemania" => "Alemania",
-	);
-
 	public function getPaises() {
 
-		return $this->paises;
+		$query = $this->db->query("SELECT * FROM pais");
+		if ($query->num_rows() > 0) {
+			return $query->result();
+		}
+		return 'No encontrado';
 	}
 }

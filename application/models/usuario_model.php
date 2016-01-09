a9ad2e5c1f51a577fce90bd2315efb33be01f41c
ukswat <mattwebb.ynwa@gmail.com>
2016-01-09 22:45:07 +0100

    Actualizado modelo de usuario
    Comprueba que el email introducido no este en la BD, y devuelve true si
    se ha podido insertar.

--- application/models/usuario_model.php
+++ application/models/usuario_model.php
@@ -4,7 +4,14 @@ class Usuario_model extends CI_Model {
 
 	public function registrarUsuario($data) {
 
+		$email = $data['email'];
+		$query = $this->db->query("SELECT email FROM usuario WHERE email = '$email'");
+		if ($query->num_rows() > 0) {
+			return FALSE;
+		}
+
 		$this->db->insert('usuario', $data);
+		return TRUE;
 	}
 
 	public function comprobarUsuarioContrasenya($email, $pass) {
